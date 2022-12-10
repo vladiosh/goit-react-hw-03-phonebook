912feccd436ebd46cf1f071dda615b648a76ff9a
VLADYSLAV FEDCHENKO <vladfedcenko33@gmail.com>
2022-12-10 10:11:47 +0200

    finish

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -43,6 +43,21 @@ class App extends Component {
     }));
   };
 
+  componentDidMount() {
+    const contacts = localStorage.getItem('contacts');
+    const parcedContacts = JSON.parse(contacts);
+
+    if (parcedContacts) {
+      this.setState({ contacts: parcedContacts });
+    }
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    if (this.state.contacts !== prevState.contacts) {
+      localStorage.setItem('contacts', JSON.stringify(this.state.contacts));
+    }
+  }
+
   render() {
     const { contacts, filter } = this.state;
 
